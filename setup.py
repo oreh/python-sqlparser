dff55616f1e40ec0f655afce989d9f625cb3ec4e
oreh <herolxh@gmail.com>
2015-12-17 06:06:14 +0000

    Use setuptools instead of distutils

--- setup.py
+++ setup.py
@@ -1,8 +1,23 @@
-# coding=utf-8
+"""A setuptools based setup module.
 
-from distutils.core import setup, Extension
-import sys
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages, Extension
+# To use a consistent encoding
+from codecs import open
 import os
+from os import path
+import sys
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 ARCH='64'
 SQLPARSER_DIR = './general_sql_parser/'
@@ -49,41 +64,37 @@ def download_library():
     print "Done!"
 
 
-if __name__ == '__main__':
-    if not os.path.isdir(SQLPARSER_DIR):
-        print "Could not find the general sql parser library in %s" % SQLPARSER_DIR
-        answer = raw_input("Do you want to download it now? (Y, N): ")
+if not os.path.isdir(SQLPARSER_DIR):
+    download_library()
 
-        if answer and answer.upper() == 'Y':
-            download_library()
+# check again (the user might have downloaded the library)
+if os.path.isdir(SQLPARSER_DIR):
+    parsebridge = Extension('sqlparser', 
+        sources = ['Parser.c', 'Statement.c', 'Node.c', 'ENodeType.c', 'parsebridgemodule.c', 
+                                    SQLPARSER_DIR + 'ext/node_visitor/node_visitor.c',
+                                    SQLPARSER_DIR + 'ext/expr_traverse/expr_traverse.c',
+                                    SQLPARSER_DIR + 'ext/modifysql/modifysql.c' ],
+        include_dirs = [ SQLPARSER_DIR + 'core/', 
+                            SQLPARSER_DIR + 'ext/collection/includes/',
+                            SQLPARSER_DIR + 'ext/expr_traverse/',
+                            SQLPARSER_DIR + 'ext/modifysql/',
+                            SQLPARSER_DIR + 'ext/node_visitor/' ],
+        library_dirs = [ SQLPARSER_DIR + '/lib/' ],
+        libraries = [ 'gspcollection', 'gspcore' ],
+        define_macros = [ ('_CRT_SECURE_NO_WARNINGS', None), ('DONT_FIX_FRAGMENTS', None), ],
+        extra_compile_args = ['-Wno-strict-prototypes'],
 
-    # check again (the user might have downloaded the library)
-    if os.path.isdir(SQLPARSER_DIR):
-        parsebridge = Extension('sqlparser', 
-            sources = ['Parser.c', 'Statement.c', 'Node.c', 'ENodeType.c', 'parsebridgemodule.c', 
-        				SQLPARSER_DIR + 'ext/node_visitor/node_visitor.c',
-        				SQLPARSER_DIR + 'ext/expr_traverse/expr_traverse.c',
-        				SQLPARSER_DIR + 'ext/modifysql/modifysql.c' ],
-            include_dirs = [ SQLPARSER_DIR + 'core/', 
-        			SQLPARSER_DIR + 'ext/collection/includes/',
-        			SQLPARSER_DIR + 'ext/expr_traverse/',
-        			SQLPARSER_DIR + 'ext/modifysql/',
-        			SQLPARSER_DIR + 'ext/node_visitor/' ],
-            library_dirs = [ SQLPARSER_DIR + '/lib/' ],
-            libraries = [ 'gspcollection', 'gspcore' ],
-            define_macros = [ ('_CRT_SECURE_NO_WARNINGS', None), ('DONT_FIX_FRAGMENTS', None), ],
-            extra_compile_args = ['-Wno-strict-prototypes'],
+    )
 
-        )
+    if sys.platform == 'win32' or sys.platform == 'win64':
+        parsebridge.extra_link_args = [ '/MANIFEST', '/DEBUG' ]
+        parsebridge.extra_compile_args = [ '/Zi' ]
 
-        if sys.platform == 'win32' or sys.platform == 'win64':
-            parsebridge.extra_link_args = [ '/MANIFEST', '/DEBUG' ]
-            parsebridge.extra_compile_args = [ '/Zi' ]
+setup (name = 'sqlparser',
+    version = '1.0',
+    description = 'A package for parsing SQL queries',
+    author = 'Timo Djurken',
+    url = 'https://github.com/TwoLaid/python-sqlparser',
+    license = 'GPL',
+    ext_modules = [ parsebridge ])
 
-        setup (name = 'sqlparser',
-            version = '1.0',
-            description = 'A package for parsing SQL queries',
-            author = 'Timo Djürken',
-            url = 'https://github.com/TwoLaid/python-sqlparser',
-            license = 'GPL',
-            ext_modules = [ parsebridge ])
